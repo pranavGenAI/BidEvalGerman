cc68624e1a33155ef41f79368241ebf4406abc0b
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2024-11-13 12:05:37 +0530

    app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def hash_password(password):
 
 # Define users and hashed passwords for simplicity
 users = {
-    "tomas": hash_password("tomas123"),
+    "user": hash_password("user"),
     "admin": hash_password("admin")
 }
 
